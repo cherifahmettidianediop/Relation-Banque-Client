d300b669fe0223fe7468bb6964093081323c62f5
cherifahmettidianediop <cherifahmettidianediop@gmail.com>
2024-12-27 15:55:10 +0000

    Update Untitled-1.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -43,3 +43,55 @@ Client* ajouterClient(Client* tete) {
     printf("Ville : ");
     scanf("%s", new->ville);
     new->next = tete;
+
+    //Menu Principal 
+   void menu() {
+    int choix;
+    do {
+        printf("\n--- MENU PRINCIPAL ---\n");
+        printf("1. Gestion des clients\n");
+        printf("2. Gestion des comptes\n");
+        printf("3. Gestion des opérations\n");
+        printf("4. Quitter\n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
+        getchar(); // Consommer le caractère de nouvelle ligne
+
+        switch (choix) {
+            case 1:
+                printf("\n1. Ajouter un client\n2. Afficher les clients\nVotre choix : ");
+                int choixClient;
+                scanf("%d", &choixClient);
+                getchar();
+                if (choixClient == 1) enregistrerClient();
+                else if (choixClient == 2) afficherClients();
+                else printf("Choix invalide !\n");
+                break;
+            case 2:
+                printf("\n1. Créer un compte\n2. Afficher les comptes\nVotre choix : ");
+                int choixCompte;
+                scanf("%d", &choixCompte);
+                getchar();
+                if (choixCompte == 1) creerCompte();
+                else if (choixCompte == 2) afficherComptes();
+                else printf("Choix invalide !\n");
+                break;
+            case 3:
+                printf("\n1. Effectuer une opération\n2. Afficher les opérations\nVotre choix : ");
+                int choixOperation;
+                scanf("%d", &choixOperation);
+                getchar();
+                if (choixOperation == 1) effectuerOperation();
+                else if (choixOperation == 2) afficherOperations();
+                else printf("Choix invalide !\n");
+                break;
+            case 4:
+                printf("Au revoir !\n");
+                break;
+            default:
+                printf("Choix invalide !\n");
+        }
+    } while (choix != 4);
+}
+
+int main() {
